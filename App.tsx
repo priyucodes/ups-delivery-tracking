55edbe4eb406f3d901bf254b08364ec6814fe56f
Priyanshu Bhandari <technopriyu@gmail.com>
2022-09-12 13:53:14 +0530

    Added queries type definition and custom hooks

--- App.tsx
+++ App.tsx
@@ -3,14 +3,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/loitering-bunny',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
